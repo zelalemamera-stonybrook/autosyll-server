d20f0960618195b04d7276adb311f26deef3ea54
zelalem <zelalem.amera.official@gmail.com>
2026-06-09 14:36:16 -0400

    server update

--- form.js
+++ form.js
@@ -9,7 +9,7 @@ const output = document.getElementById("output");
 async function syllabify( ){
 	output.value = 'processing your input ... ';
 	console.log(textarea.value);
-	const response = await fetch('http://autosyll/proper_syllables.txt', {
+	const response = await fetch('https://main.d1rvmsp3b35yzu.amplifyapp.com/proper_syllables.txt', {
 		method: 'POST',
 		headers: {'Content-Type': 'text/plain',},
 		body: textarea.value,
